d4ce20455ede0f153467f5fe0a1e8abd1539c084
Chris Wright <chris.wright@nanoporetech.com>
2020-09-23 12:42:51 +0100

    Customize SEO title

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ const BlogIndex = ({ data, location }) => {
   if (posts.length === 0) {
     return (
       <Layout location={location} title={siteTitle}>
-        <SEO title="All posts" />
+        <SEO title="Oxford Nanopore Open Datasets" />
         <Bio />
         <p>No blog posts found. Add markdown posts to "content/blog" (or the directory you specified for the "gatsby-source-filesystem" plugin in gatsby-config.js).</p>
       </Layout>
@@ -22,7 +22,7 @@ const BlogIndex = ({ data, location }) => {
 
   return (
     <Layout location={location} title={siteTitle}>
-      <SEO title="All posts" />
+      <SEO title="Oxford Nanopore Open Datasets" />
       <Bio />
       {posts.map((post) => {
         const title = post.frontmatter.title || post.fields.slug
